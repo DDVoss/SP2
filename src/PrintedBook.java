b4361432ed1b6e179b535c0ea282216d283fa9b9
DDVoss <danieldvoss@gmail.com>
2024-10-29 12:10:13 +0100

    method and variables
    added methods and variables that corresponds to the class diagram in the assignement.

---
+++ src/PrintedBook.java
@@ -0,0 +1,18 @@
+public class PrintedBook extends Title {
+    private int pages;
+
+    PrintedBook(String title, String literatureType, int copies, int pages)   {
+        super(title, literatureType, copies);
+        this.pages = pages;
+    }
+
+    @Override
+    public double calculatePoints() {
+        return 0;
+    }
+
+    @Override
+    public double calculateLiteraturePoints() {
+        return 0;
+    }
+}
